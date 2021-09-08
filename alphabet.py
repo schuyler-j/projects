e3e28732a8f6e2336e5d75c9b5313e7f762037ab
Jay <schu0527@flinders.edu.au>
2021-09-08 10:57:14 +0930

    pyfun

---
+++ alphabet.py
@@ -0,0 +1,140 @@
+
+
+word = 'cat'
+#passes = int(input("How many passes? "))
+word = word.upper()
+
+alphabet = {
+    "A":10025,
+    "B":10124,
+    "C":10223,
+    "D":10322,
+    "E":10421,
+    "F":10520,
+    "G":10619,
+    "H":10718,
+    "I":10817,
+    "J":10916,
+    "K":11015,
+    "L":11114,
+    "M":11213,
+    "N":11312,
+    "O":11411,
+    "P":11510,
+    "Q":11609,
+    "R":11708,
+    "S":11807,
+    "T":11906,
+    "U":12005,
+    "V":12104,
+    "W":12203,
+    "X":12302,
+    "Y":12401,
+    "Z":12500,
+}
+
+alphabet_ref = {
+    10025:"A",
+    10124:"B",
+    10223:"C",
+    10322:"D",
+    10421:"E",
+    10520:"F",
+    10619:"G",
+    10718:"H",
+    10817:"I",
+    10916:"J",
+    11015:"K",
+    11114:"L",
+    11213:"M",
+    11312:"N",
+    11411:"O",
+    11510:"P",
+    11609:"Q",
+    11708:"R",
+    11807:"S",
+    11906:"T",
+    12005:"U",
+    12104:"V",
+    12203:"W",
+    12302:"X",
+    12401:"Y",
+    12500:"Z",
+}
+
+keys = list(alphabet)
+
+neighbour = []
+j = [] 
+
+
+k = 99 #constant displacement
+count = 0
+flip = 0 
+for i in word:
+    #if count < len(word)-1:
+    nextL = alphabet[word[count]]
+    nextL = nextL % 100
+
+    i = alphabet[i]
+    a = i % 100
+
+    #if i > 10900:
+    #    i = i % 10000
+    #else:
+    #    i = i % 1000 
+    #i = i // 100
+
+
+    count = count + 1
+
+    #i = i + passes
+
+    passes = a - nextL
+
+    supply = 7*k  
+
+    #for x in range(len(word)):
+
+        #i = i + passes
+        #if i > 12:
+        #    i = i - passes - x
+        #else:
+        #    i = i + passes + x
+        #j.append(i)
+
+    if flip == 1:
+        meal =6*k + passes*k
+    else:
+        meal = 6*k
+    i = i + meal 
+
+    j.append(i)
+
+
+    flip = 1
+
+
+    #i = keys[i]
+    #print(i)
+    
+#for r in range(len(j)):
+#    if j[r] < 10:
+#        j[r] = str("10") + str(j[r]) + str(25-j[r])
+#    elif 25-j[r] < 10:
+#        j[r] = str("1") + str(j[r]) + str("0") +str(25-j[r])
+#    else:
+#        j[r] = str("1") + str(j[r]) + str(25-j[r])
+
+for r in j:
+    print(alphabet_ref[r], end="")
+#space
+print("")
+
+print(j)
+#cast all to str
+for z in range(len(j)):
+    j[z] = str(j[z])
+
+z = ''.join(j)
+print(z)
---
+++ keyer.py
@@ -0,0 +1,74 @@
+import random
+from time import sleep
+import win32
+import keyboard
+from win32 import win32clipboard
+import io
+from random import randint, shuffle
+import re
+
+
+
+search = ""
+
+
+    #keyboard.send("ctrl+v, enter")
+
+j = []
+a=1
+while a == 1:
+    #Gif picker
+    print("hello")
+    num = randint(0, 6)
+    sleep(5) #give time to run away
+    #with open('poc.txt', 'r') as file:
+    #    data = file.readlines()
+    with open('poc1.txt', 'r') as file:
+        data = file.readlines()
+
+
+    if num % 2 == 0:
+        txt = data[num]
+        x = re.findall("[a-z]{1}", txt)
+        x = ''.join(x)
+        print(x)
+        x = re.findall("[a-z]{3}", x)
+
+        b = randint(0, len(x)-1)
+        print(x[b])
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardText(x[b])
+        win32clipboard.CloseClipboard()
+
+        keyboard.send("ctrl+g")
+        sleep(0.5)
+        keyboard.send("ctrl+v")
+        sleep(2)
+        keyboard.send("down")
+        sleep(0.5)
+        keyboard.send("enter")
+    else:
+        num = randint(0, 32)
+        search = data[num]
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardText("@skazz")
+        win32clipboard.CloseClipboard()
+
+        keyboard.send("ctrl+v, space")
+
+        sleep(1)
+
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardText(search)
+        win32clipboard.CloseClipboard()
+
+        keyboard.send("ctrl+v")
+        keyboard.send("enter")
+
+        sleep(0.5)
+
+
+
