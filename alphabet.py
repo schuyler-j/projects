80df203d7c60cc1e954b51562a1f6d81eb908751
Jay <schu0527@flinders.edu.au>
2021-09-09 12:23:27 +0930

    back to old

--- alphabet.py
+++ alphabet.py
@@ -1,6 +1,6 @@
 
 
-word = 'cat'
+word = input("Word to encrypt? ") 
 #passes = int(input("How many passes? "))
 word = word.upper()
 
@@ -34,32 +34,32 @@ alphabet = {
 }
 
 alphabet_ref = {
-    10025:"A",
-    10124:"B",
-    10223:"C",
-    10322:"D",
-    10421:"E",
-    10520:"F",
-    10619:"G",
-    10718:"H",
-    10817:"I",
-    10916:"J",
-    11015:"K",
-    11114:"L",
-    11213:"M",
-    11312:"N",
-    11411:"O",
-    11510:"P",
-    11609:"Q",
-    11708:"R",
-    11807:"S",
-    11906:"T",
-    12005:"U",
-    12104:"V",
-    12203:"W",
-    12302:"X",
-    12401:"Y",
-    12500:"Z",
+    '10025':"A",
+    '10124':"B",
+    '10223':"C",
+    '10322':"D",
+    '10421':"E",
+    '10520':"F",
+    '10619':"G",
+    '10718':"H",
+    '10817':"I",
+    '10916':"J",
+    '11015':"K",
+    '11114':"L",
+    '11213':"M",
+    '11312':"N",
+    '11411':"O",
+    '11510':"P",
+    '11609':"Q",
+    '11708':"R",
+    '11807':"S",
+    '11906':"T",
+    '12005':"U",
+    '12104':"V",
+    '12203':"W",
+    '12302':"X",
+    '12401':"Y",
+    '12500':"Z",
 }
 
 keys = list(alphabet)
@@ -71,60 +71,73 @@ j = []
 k = 99 #constant displacement
 count = 0
 flip = 0 
+passes = 5
+
+
+word = word.split() 
+
+
+
 for i in word:
-    #if count < len(word)-1:
-    nextL = alphabet[word[count]]
-    nextL = nextL % 100
-
-    i = alphabet[i]
-    a = i % 100
-
-    #if i > 10900:
-    #    i = i % 10000
-    #else:
-    #    i = i % 1000 
-    #i = i // 100
-
-
+    c = word[count]
     count = count + 1
+    for b in c:
+        #nextL = alphabet[word[count+1]]
+        #nextL = nextL % 100
+        #print(nextL)
 
-    #i = i + passes
+        i = alphabet[b]
+        a = i % 100
 
-    passes = a - nextL
+        
+        if i > 10900:
+        
+            i = i % 10000
+        
+        else:
+        
+            i = i % 1000 
+        
+        i = i // 100
 
-    supply = 7*k  
 
-    #for x in range(len(word)):
 
         #i = i + passes
-        #if i > 12:
-        #    i = i - passes - x
+
+        #passes = a - nextL
+
+
+        for x in range(len(word)):
+
+            i = i + passes
+            if i > 12:
+                i = i - passes - x
+            else:
+                i = i + passes + x
+            j.append(i)
+
+        #if flip == 1:
+        #    meal =6*k + passes*k
         #else:
-        #    i = i + passes + x
+        #    meal = 6*k
+        #i = i + meal 
+
         #j.append(i)
 
-    if flip == 1:
-        meal =6*k + passes*k
-    else:
-        meal = 6*k
-    i = i + meal 
 
-    j.append(i)
+        #flip = 1
 
 
-    flip = 1
-
-
-    #i = keys[i]
-    #print(i)
+        i = keys[i]
+        print(i)
     
-#for r in range(len(j)):
-#    if j[r] < 10:
-#        j[r] = str("10") + str(j[r]) + str(25-j[r])
-#    elif 25-j[r] < 10:
-#        j[r] = str("1") + str(j[r]) + str("0") +str(25-j[r])
-#    else:
-#        j[r] = str("1") + str(j[r]) + str(25-j[r])
+for r in range(len(j)):
+    if j[r] < 10:
+        j[r] = str("10") + str(j[r]) + str(25-j[r])
+    elif 25-j[r] < 10:
+        j[r] = str("1") + str(j[r]) + str("0") +str(25-j[r])
+    else:
+        j[r] = str("1") + str(j[r]) + str(25-j[r])
 
 for r in j:
     print(alphabet_ref[r], end="")
